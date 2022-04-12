06f526414c48ae0c478e3ab1c0cef05b8e3f3301
Anna Tuban <mshannahstarling@gmail.com>
2022-04-12 18:16:39 +0300

    fix: change map center

--- src/components/Map/constants.js
+++ src/components/Map/constants.js
@@ -1,4 +1,4 @@
-export const MOSCOW_CENTER = [55.7543627209379, 37.753664781640296];
+export const MOSCOW_CENTER = [55.76269367367688, 38.07355575522396];
 export const PATH_POLYLINE = {
   color: 'blue',
   opacity: 0.6,
